2afb7af12135805a9afab34b333c2c02ffa0930b
spolischook <spolischook@gmail.com>
2013-02-03 00:55:58 +0200

    Installed StofDoctrineExtensionsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Geekhub\DreamBundle\DreamBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
