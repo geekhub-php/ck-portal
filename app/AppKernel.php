1fa1e117c8e16b7fca36e58a521ca5aac0f4be97
spolischook <spolischook@gmail.com>
2013-02-03 13:06:21 +0200

    Enable TagBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,7 @@ class AppKernel extends Kernel
             new Geekhub\DreamBundle\DreamBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FPN\TagBundle\FPNTagBundle(),
+            new Geekhub\TagBundle\TagBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
