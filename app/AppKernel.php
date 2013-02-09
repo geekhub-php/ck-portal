349ae31b3fb9c18b314cbfe2a643149c68f3938e
spolischook <spolischook@gmail.com>
2013-02-10 00:35:02 +0200

    Enable UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FPN\TagBundle\FPNTagBundle(),
             new Geekhub\TagBundle\TagBundle(),
+            new Geekhub\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
