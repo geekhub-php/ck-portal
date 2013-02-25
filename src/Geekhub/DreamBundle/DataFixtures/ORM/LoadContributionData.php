59c5af8164778c40d2f4886da703d3b04880e8c9
monobook <mad.monax@gmail.com>
2013-02-25 12:56:40 +0200

    added Contribution fixtures

---
+++ src/Geekhub/DreamBundle/DataFixtures/ORM/LoadContributionData.php
@@ -0,0 +1,39 @@
+<?php
+namespace Geekhub\DreamBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Geekhub\DreamBundle\Entity\ContributorSupport;
+
+class LoadContributionData extends AbstractFixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $point = array('work', 'financial', 'equipment');
+
+        for ($i = 1; $i <= 100; $i++) {
+            $contribution = new ContributorSupport();
+            $contribution->setDream($this->getReference('dream'.rand(1,30)));
+            //$contribution->setPoint($this->getReference($point[rand(0,2)].$i));
+            $contribution->setHide(rand(0,1));
+            $contribution->setUser($this->getReference('user'.rand(1,17)));
+
+            $manager->persist($contribution);
+            $manager->flush();
+
+        }
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 32;
+    }
+
+
+}
