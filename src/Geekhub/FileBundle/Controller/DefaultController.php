db2815c81cfd0ca08c02d7b9b6a1b6f69eff4ec1
spolischook <spolischook@gmail.com>
2013-03-09 02:46:53 +0200

    Update upload controller

--- src/Geekhub/FileBundle/Controller/DefaultController.php
+++ src/Geekhub/FileBundle/Controller/DefaultController.php
@@ -5,23 +5,44 @@ namespace Geekhub\FileBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Gedmo\Uploadable\UploadableListener;
+
+use Geekhub\FileBundle\Entity\Image;
+use Geekhub\FileBundle\Entity\Document;
 
 class DefaultController extends Controller
 {
-    public function uploadFileAction()
+    public function uploadImageAction(Request $request)
     {
+        //ToDo Add user check
+        $image = new Image();
+
+        $image->setAllowedMimeType($this->container->getParameter('geekhub_file.image.allowed_mime_type'));
+        $image->setSizeLimit($this->container->getParameter('geekhub_file.image.size_limit'));
+        $image->setUploadDir($this->container->getParameter('geekhub_file.image.upload_directory'));
+
         $fileUploader = $this->get('geekhub.file_bundle.file_uploader');
+        $fileUploader->handleUpload($request, $image);
 
-        $fileUploader->allowedExtensions = array();
-        $fileUploader->sizeLimit = 10 * 1024 * 1024;
-        $fileUploader->inputName = 'qqfile';
-        $fileUploader->chunksFolder = 'chunks';
-        $result = $fileUploader->handleUpload('uploads');
-        $result['uploadName'] = $fileUploader->getUploadName();
-        $result['mimeType'] = $fileUploader->getMimeType('uploads');
-        $result['fileSize'] = $fileUploader->getFileSize('uploads');
-        $result =  json_encode($result);
+        $serializedImage = $this->container->get('serializer')->serialize($image, 'json');
 
-        return new Response($result, 200, array('Content-Type' => 'text/plain'));
+        return new Response($serializedImage, 200, array('Content-Type' => 'text/plain'));
+    }
+
+    public function uploadDocumentAction(Request $request)
+    {
+        //ToDo Add user check
+        $document = new Document();
+
+        $document->setAllowedMimeType($this->container->getParameter('geekhub_file.document.allowed_mime_type'));
+        $document->setSizeLimit($this->container->getParameter('geekhub_file.document.size_limit'));
+        $document->setUploadDir($this->container->getParameter('geekhub_file.document.upload_directory'));
+
+        $fileUploader = $this->get('geekhub.file_bundle.file_uploader');
+        $fileUploader->handleUpload($request, $document);
+
+        $serializedDocument = $this->container->get('serializer')->serialize($document, 'json');
+
+        return new Response($serializedDocument, 200, array('Content-Type' => 'text/plain'));
     }
 }
