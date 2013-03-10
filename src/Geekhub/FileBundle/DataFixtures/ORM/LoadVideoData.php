49e65699cc6e00e9ac58f7ac7d21878540f62cd6
spolischook <spolischook@gmail.com>
2013-03-10 23:17:28 +0200

    Updated fixtures

--- src/Geekhub/FileBundle/DataFixtures/ORM/LoadVideoData.php
+++ src/Geekhub/FileBundle/DataFixtures/ORM/LoadVideoData.php
@@ -5,26 +5,37 @@ use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Geekhub\FileBundle\Entity\Video;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class LoadVideoData extends AbstractFixture implements OrderedFixtureInterface
+class LoadVideoData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
+    /** @var ContainerInterface */
+    private $container;
+
     public function load(ObjectManager $manager)
     {
+        $destDir = $this->container->getParameter('geekhub_file.video.upload_directory');
+        $currentFile = $this->container->get('kernel')->getRootDir() . '/../web/' .
+            $destDir . 'video.jpg';
+        copy(__DIR__ . '/srcFile/video.jpg', $currentFile);
         $list = $this->getVideoList();
         $type = array('vimeo', 'youtube');
 
         for ($i = 0; $i < 100; $i++) {
             $video = new Video();
             $currentType = $type[rand(0,1)];
+            $currentLink = $list[rand(1, 30)][$currentType];
 
             $video->setType($currentType);
-            $video->setLink($list[rand(1, 30)][$currentType]);
+            $video->setLink($currentLink);
+            $video->setThumbnail($destDir . 'video.jpg');
+            $video->setRemoteThumbnail("http://www.direct-media.ru/ck_file/images/4.jpg");
 
             $manager->persist($video);
             $manager->flush();
 
             $this->addReference('video' . $i, $video);
-
         }
     }
 
@@ -44,6 +55,11 @@ class LoadVideoData extends AbstractFixture implements OrderedFixtureInterface
         fclose($youtubeFile);
 
         return $list;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 
     /**
