e7ecace949fafc27a20c5c8b396c87a421f76b50
spolischook <spolischook@gmail.com>
2013-03-11 18:53:03 +0200

    Updated entities

--- src/Geekhub/DreamBundle/Entity/Equipment.php
+++ src/Geekhub/DreamBundle/Entity/Equipment.php
@@ -10,166 +10,23 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="equipment")
  * @ORM\Entity
  */
-class Equipment
+class Equipment extends AbstractPoint
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="item", type="string", length=255)
-     */
-    protected $item;
-
-    /**
-     * @var integer
-     *
-     * @ORM\ManyToOne(targetEntity="Unit")
-     */
-    protected $unit;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="total", type="integer")
-     */
-    protected $total;
-
     /** @ORM\ManyToOne(targetEntity="Dream", inversedBy="equipment") */
     protected $dream;
 
-    /** @ORM\ManyToOne(targetEntity="ContributorSupport", inversedBy="equipment") */
-    protected $contribution;
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set item
-     *
-     * @param string $item
-     * @return Equipment
-     */
-    public function setItem($item)
-    {
-        $this->item = $item;
-    
-        return $this;
-    }
-
-    /**
-     * Get item
-     *
-     * @return string 
-     */
-    public function getItem()
-    {
-        return $this->item;
-    }
-
-    /**
-     * Set total
-     *
-     * @param integer $total
-     * @return Equipment
-     */
-    public function setTotal($total)
-    {
-        $this->total = $total;
-    
-        return $this;
-    }
-
-    /**
-     * Get total
-     *
-     * @return integer 
-     */
-    public function getTotal()
-    {
-        return $this->total;
-    }
-
-    /**
-     * Set unit
-     *
-     * @param \Geekhub\DreamBundle\Entity\Unit $unit
-     * @return Equipment
-     */
-    public function setUnit(\Geekhub\DreamBundle\Entity\Unit $unit = null)
-    {
-        $this->unit = $unit;
-    
-        return $this;
-    }
-
-    /**
-     * Get unit
-     *
-     * @return \Geekhub\DreamBundle\Entity\Unit 
-     */
-    public function getUnit()
-    {
-        return $this->unit;
-    }
-
-    /**
-     * Set dream
-     *
-     * @param \Geekhub\DreamBundle\Entity\Dream $dream
-     * @return Equipment
-     */
-    public function setDream(\Geekhub\DreamBundle\Entity\Dream $dream = null)
+    public function setDream($dream)
     {
         $this->dream = $dream;
-    
-        return $this;
     }
 
-    /**
-     * Get dream
-     *
-     * @return \Geekhub\DreamBundle\Entity\Dream 
-     */
     public function getDream()
     {
         return $this->dream;
     }
 
-    /**
-     * Set contribution
-     *
-     * @param \Geekhub\DreamBundle\Entity\ContributorSupport $contribution
-     * @return Equipment
-     */
-    public function setContribution(\Geekhub\DreamBundle\Entity\ContributorSupport $contribution = null)
+    public function __toString()
     {
-        $this->contribution = $contribution;
-    
-        return $this;
-    }
-
-    /**
-     * Get contribution
-     *
-     * @return \Geekhub\DreamBundle\Entity\ContributorSupport 
-     */
-    public function getContribution()
-    {
-        return $this->contribution;
+        return 'Equipment';
     }
 }
--- src/Geekhub/DreamBundle/Entity/Work.php
+++ src/Geekhub/DreamBundle/Entity/Work.php
@@ -10,166 +10,46 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="work")
  * @ORM\Entity
  */
-class Work
+class Work extends AbstractPoint
 {
     /**
      * @var integer
      *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="worker", type="integer")
      */
-    protected $id;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="job", type="string", length=255)
-     */
-    protected $job;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="employee", type="integer")
-     */
-    protected $employee;
-
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="day", type="integer")
-     */
-    protected $day;
+    protected $worker;
 
     /** @ORM\ManyToOne(targetEntity="Dream", inversedBy="work") */
     protected $dream;
 
-    /** @ORM\ManyToOne(targetEntity="ContributorSupport", inversedBy="work") */
-    protected $contribution;
-
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set job
-     *
-     * @param string $job
-     * @return Work
-     */
-    public function setJob($job)
-    {
-        $this->job = $job;
-    
-        return $this;
-    }
-
-    /**
-     * Get job
-     *
-     * @return string 
-     */
-    public function getJob()
-    {
-        return $this->job;
-    }
-
-    /**
-     * Set employee
-     *
-     * @param integer $employee
-     * @return Work
-     */
-    public function setEmployee($employee)
-    {
-        $this->employee = $employee;
-    
-        return $this;
-    }
-
-    /**
-     * Get employee
-     *
-     * @return integer 
-     */
-    public function getEmployee()
-    {
-        return $this->employee;
-    }
-
-    /**
-     * Set day
-     *
-     * @param integer $day
-     * @return Work
-     */
-    public function setDay($day)
-    {
-        $this->day = $day;
-    
-        return $this;
-    }
-
-    /**
-     * Get day
-     *
-     * @return integer 
-     */
-    public function getDay()
-    {
-        return $this->day;
-    }
-
-    /**
-     * Set dream
-     *
-     * @param \Geekhub\DreamBundle\Entity\Dream $dream
-     * @return Work
-     */
-    public function setDream(\Geekhub\DreamBundle\Entity\Dream $dream = null)
+    public function setDream($dream)
     {
         $this->dream = $dream;
-    
-        return $this;
     }
 
-    /**
-     * Get dream
-     *
-     * @return \Geekhub\DreamBundle\Entity\Dream 
-     */
     public function getDream()
     {
         return $this->dream;
     }
 
     /**
-     * Set contribution
-     *
-     * @param \Geekhub\DreamBundle\Entity\ContributorSupport $contribution
-     * @return Work
+     * @param int $worker
      */
-    public function setContribution(\Geekhub\DreamBundle\Entity\ContributorSupport $contribution = null)
+    public function setWorker($worker)
     {
-        $this->contribution = $contribution;
-    
-        return $this;
+        $this->worker = $worker;
     }
 
     /**
-     * Get contribution
-     *
-     * @return \Geekhub\DreamBundle\Entity\ContributorSupport 
+     * @return int
      */
-    public function getContribution()
+    public function getWorker()
     {
-        return $this->contribution;
+        return $this->worker;
+    }
+
+    public function __toString()
+    {
+        return 'Work';
     }
 }
