1b82aa51308e2b60571dfc1d07501538496dcc83
spolischook <spolischook@gmail.com>
2013-03-16 16:49:29 +0200

    Add new method for recalculation ProgressBar at DreamManager

--- src/Geekhub/DreamBundle/DreamManager.php
+++ src/Geekhub/DreamBundle/DreamManager.php
@@ -72,4 +72,53 @@ class DreamManager
 
         return $contributorsArray;
     }
+
+    public function getNewProgressBar(Dream $dream)
+    {
+        $planQuantity = 0;
+        $partPlan = 0;
+        $progressBar = $dream->getProgressBar();
+
+        //Set finance
+        foreach ($dream->getFinancial() as $financialPoint) {
+            if ($financialPoint->getIsDonate() == false) {
+                $planQuantity = $planQuantity + $financialPoint->getQuantity();
+            }
+            else {
+                $partPlan = $partPlan + $financialPoint->getQuantity();
+            }
+        }
+
+        $progressBar->setFinance(round(($partPlan/$planQuantity), 2));
+
+        //Set equipment
+        $planQuantity = 0;
+        $partPlan = 0;
+        foreach ($dream->getEquipment() as $equipmentPoint) {
+            if ($equipmentPoint->getIsDonate() == false) {
+                $planQuantity = $planQuantity + $equipmentPoint->getQuantity();
+            }
+            else {
+                $partPlan = $partPlan + $equipmentPoint->getQuantity();
+            }
+        }
+
+        $progressBar->setEquipment(round(($partPlan/$planQuantity), 2));
+
+        //Set work
+        $planQuantity = 0;
+        $partPlan = 0;
+        foreach ($dream->getWork() as $workPoint) {
+            if ($workPoint->getIsDonate() == false) {
+                $planQuantity = $planQuantity + $workPoint->getQuantity() * $workPoint->getWorker();
+            }
+            else {
+                $partPlan = $partPlan + $workPoint->getQuantity();
+            }
+        }
+
+        $progressBar->setWork(round(($partPlan/$planQuantity), 2));
+
+        return $progressBar;
+    }
 }
