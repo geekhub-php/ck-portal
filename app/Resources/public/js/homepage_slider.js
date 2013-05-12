9d073139ac93482b0af6ec7e13ce590808405295
irene <irene@irene-R538.(none)>
2013-05-12 15:38:35 +0300

    fix for homepage images & slider

--- app/Resources/public/js/homepage_slider.js
+++ app/Resources/public/js/homepage_slider.js
@@ -1,21 +1,35 @@
 jQuery(document).ready(function() {
+    window.change_slides = 1;
     jQuery('.slider ul li:first-child').addClass('current');
-    jQuery('a#dream-1').addClass('active');
+    jQuery('a#1').addClass('active');
     setTimeout("changeSlides(1);", 5000);
 });
 
-function changeSlides($i) {
-    jQuery('.slider ul li').removeClass('current');
-    jQuery('a#dream-'+$i).removeClass('active');
+jQuery('.splash li a').click(function() {
+    var $i = jQuery(this).attr('id');
+    window.change_slides = 0;
+    jQuery('.slider ul li a').removeClass('current');
+    jQuery('.splash li a').removeClass('active');
+    jQuery('.slider li:nth-child('+$i+') a').addClass('current');
+    jQuery('a#'+$i).addClass('active');
 
-    if ($i == 4) {
-        jQuery('.slider ul li:first-child').addClass('current');
-        jQuery('a#dream-1').addClass('active');
-        $i = 1;
-    } else {
-        $i++;
-        jQuery('.slider ul li:nth-child('+$i+')').addClass('current');
-        jQuery('a#dream-'+$i).addClass('active');
+});
+
+function changeSlides($i) {
+    if (window.change_slides != 0) {
+        jQuery('.slider ul li').removeClass('current');
+        jQuery('a#'+$i).removeClass('active');
+
+        if ($i == 4) {
+            jQuery('.slider ul li:first-child').addClass('current');
+            jQuery('a#1').addClass('active');
+            $i = 1;
+        } else {
+            $i++;
+            jQuery('.slider ul li:nth-child('+$i+')').addClass('current');
+            jQuery('a#'+$i).addClass('active');
+        }
+        setTimeout('changeSlides('+$i+');', 5000);
     }
-    setTimeout('changeSlides('+$i+');', 5000);
+
 }
