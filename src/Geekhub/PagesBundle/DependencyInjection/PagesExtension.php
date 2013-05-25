3f05ead9751035733399514794ac52b803b51fb9
SPolischook <spolischook@gmail.com>
2013-05-25 13:52:15 +0300

    Include created servises in container

--- src/Geekhub/PagesBundle/DependencyInjection/PagesExtension.php
+++ src/Geekhub/PagesBundle/DependencyInjection/PagesExtension.php
@@ -22,5 +22,7 @@ class PagesExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('admin.xml');
     }
 }
