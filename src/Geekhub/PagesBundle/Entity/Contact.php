8cc68846b6e9cb495352c2c54d306d56c6e2cc66
SPolischook <spolischook@gmail.com>
2013-05-25 13:49:06 +0300

    Update Entity Contact

--- src/Geekhub/PagesBundle/Entity/Contact.php
+++ src/Geekhub/PagesBundle/Entity/Contact.php
@@ -2,16 +2,22 @@
 
 namespace Geekhub\PagesBundle\Entity;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * Contacts
  *
  * @ORM\Table(name="contact")
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  */
 class Contact
 {
+    private $temp;
+
     /**
      * @var integer
      *
@@ -25,15 +31,19 @@ class Contact
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank
      */
     private $name;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="image", type="string", length=255)
+     * @Assert\File(maxSize="6000000")
      */
-    private $image;
+    private $file;
+
+    /**
+     * @ORM\Column(name="imagePath", type="string", length=255, nullable=true)
+     */
+    public $imagePath;
 
     /**
      * @var string
@@ -41,6 +51,12 @@ class Contact
      * @ORM\Column(name="contact", type="string", length=255)
      */
     private $contact;
+
+    /**
+     * @Gedmo\SortablePosition
+     * @ORM\Column(name="position", type="integer")
+     */
+    private $position;
 
 
     /**
@@ -77,29 +93,6 @@ class Contact
     }
 
     /**
-     * Set image
-     *
-     * @param string $image
-     * @return Contacts
-     */
-    public function setImage($image)
-    {
-        $this->image = $image;
-
-        return $this;
-    }
-
-    /**
-     * Get image
-     *
-     * @return string 
-     */
-    public function getImage()
-    {
-        return $this->image;
-    }
-
-    /**
      * Set contact
      *
      * @param string $contact
@@ -120,5 +113,126 @@ class Contact
     public function getContact()
     {
         return $this->contact;
+    }
+
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    public function setPosition($position)
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->imagePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->imagePath;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->imagePath
+            ? null
+            : $this->getUploadDir().'/'.$this->imagePath;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/contacts';
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->imagePath)) {
+            // store the old name to delete after the update
+            $this->temp = $this->imagePath;
+            $this->imagePath = null;
+        } else {
+            $this->imagePath = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->imagePath = $filename.'.'.$this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getFile()->move($this->getUploadRootDir(), $this->imagePath);
+
+        // check if we have an old image
+        if (isset($this->temp)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->name ? $this->name : 'New contact';
     }
 }
