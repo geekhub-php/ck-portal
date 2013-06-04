0c522840e76397bdb9d6abf2dae492d1e3da21f7
SPolischook <spolischook@gmail.com>
2013-06-04 18:08:06 +0300

    Create contact form

--- src/Geekhub/PagesBundle/Controller/DefaultController.php
+++ src/Geekhub/PagesBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Geekhub\PagesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -14,14 +15,46 @@ class DefaultController extends Controller
         ));
     }
 
-    public function contactsAction()
+    public function contactsAction(Request $request)
     {
         $contacts = $this->getDoctrine()->
             getManager()->
             getRepository('PagesBundle:Contact')->
             findBy(array(), array('position' => 'ASC'));
+
+        $form = $this->createFormBuilder()
+            ->add('email', 'email', array('label' => 'Адреса для зворотнього зв’язку'))
+            ->add('theme', 'text', array('label' => 'Тема листа'))
+            ->add('text', 'textarea', array('label' => 'Текст повідомлення'))
+            ->add('captcha', 'text', array('label' => 'Капча'))
+            ->getForm();
+        ;
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+
+                $formData = $form->getData();
+//                var_dump($formData); exit;
+                $message = \Swift_Message::newInstance()
+                    ->setSubject($formData['theme'])
+                    ->setFrom($formData['email'])
+                    ->setTo($this->container->getParameter('delivery_address'))
+                    ->setBody($formData['text'])
+                ;
+                $this->get('mailer')->send($message);
+
+                return $this->render('PagesBundle:Default:contacts.html.twig', array(
+                    'contacts' => $contacts,
+                    'form'     => $form->createView()
+                ));
+            }
+        }
+
         return $this->render('PagesBundle:Default:contacts.html.twig', array(
             'contacts' => $contacts,
+            'form'     => $form->createView()
         ));
     }
 }
