5759772dbcafca1fe2deb2e6d6de6ddda13f5f62
SPolischook <spolischook@gmail.com>
2013-06-04 16:59:22 +0300

    Update Address Entity

--- src/Geekhub/PagesBundle/Entity/Address.php
+++ src/Geekhub/PagesBundle/Entity/Address.php
@@ -2,6 +2,8 @@
 
 namespace Geekhub\PagesBundle\Entity;
 
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -9,6 +11,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="address")
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  */
 class Address
 {
@@ -29,11 +32,14 @@ class Address
     private $name;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="image", type="string", length=255)
+     * @Assert\File(maxSize="6000000")
      */
-    private $image;
+    private $file;
+
+    /**
+     * @ORM\Column(name="imagePath", type="string", length=255, nullable=true)
+     */
+    public $imagePath;
 
     /**
      * @var string
@@ -88,29 +94,6 @@ class Address
     public function getName()
     {
         return $this->name;
-    }
-
-    /**
-     * Set image
-     *
-     * @param string $image
-     * @return Address
-     */
-    public function setImage($image)
-    {
-        $this->image = $image;
-
-        return $this;
-    }
-
-    /**
-     * Get image
-     *
-     * @return string 
-     */
-    public function getImage()
-    {
-        return $this->image;
     }
 
     /**
@@ -180,5 +163,121 @@ class Address
     public function getLongitude()
     {
         return $this->longitude;
+    }
+
+    public function getMap()
+    {
+        return ($this->getLatitude() && $this->getLongitude())
+            ? array('latitude' => $this->getLatitude(), 'longitude' => $this->getLongitude())
+            : false;
+    }
+
+    public function getAbsolutePath()
+    {
+        return null === $this->imagePath
+            ? null
+            : $this->getUploadRootDir().'/'.$this->imagePath;
+    }
+
+    public function getWebPath()
+    {
+        return null === $this->imagePath
+            ? null
+            : $this->getUploadDir().'/'.$this->imagePath;
+    }
+
+    protected function getUploadRootDir()
+    {
+        // the absolute directory path where uploaded
+        // documents should be saved
+        return __DIR__.'/../../../../web/'.$this->getUploadDir();
+    }
+
+    protected function getUploadDir()
+    {
+        // get rid of the __DIR__ so it doesn't screw up
+        // when displaying uploaded doc/image in the view.
+        return 'uploads/address';
+    }
+
+    /**
+     * Sets file.
+     *
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+        // check if we have an old image path
+        if (isset($this->imagePath)) {
+            // store the old name to delete after the update
+            $this->temp = $this->imagePath;
+            $this->imagePath = null;
+        } else {
+            $this->imagePath = 'initial';
+        }
+    }
+
+    /**
+     * Get file.
+     *
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function preUpload()
+    {
+        if (null !== $this->getFile()) {
+            // do whatever you want to generate a unique name
+            $filename = sha1(uniqid(mt_rand(), true));
+            $this->imagePath = $filename.'.'.$this->getFile()->guessExtension();
+        }
+    }
+
+    /**
+     * @ORM\PostPersist()
+     * @ORM\PostUpdate()
+     */
+    public function upload()
+    {
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // if there is an error when moving the file, an exception will
+        // be automatically thrown by move(). This will properly prevent
+        // the entity from being persisted to the database on error
+        $this->getFile()->move($this->getUploadRootDir(), $this->imagePath);
+
+        // check if we have an old image
+        if (isset($this->temp)) {
+            // delete the old image
+            unlink($this->getUploadRootDir().'/'.$this->temp);
+            // clear the temp image path
+            $this->temp = null;
+        }
+        $this->file = null;
+    }
+
+    /**
+     * @ORM\PostRemove()
+     */
+    public function removeUpload()
+    {
+        if ($file = $this->getAbsolutePath()) {
+            unlink($file);
+        }
+    }
+
+    public function __toString()
+    {
+        return $this->name ? $this->name : '' ;
     }
 }
